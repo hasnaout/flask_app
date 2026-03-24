503cccb7df8717353d19f06453800a3793e9f70d
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-03-24 16:04:44 +0100

    changement de fichier app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-  return "Hello Devops -Kocker CI Pipline! Partie 3"
+  return "Hello Devops -Kocker CI Pipline! Partie 4"
 
 
 if __name__ =="__main__":
